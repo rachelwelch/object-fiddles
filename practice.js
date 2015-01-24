a265aebf0e805ea31d1004b6239ae54f9daa1722
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-01-24 11:00:31 -0700

    complete first 4 problems in practice.js

--- practice.js
+++ practice.js
@@ -3,32 +3,30 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
-
-
+  var me = {name: "Rachel", age: 28};
+  alert(me.name);
 
 
 //NEXT PROBLEM
 
 
-
-
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+  var favoriteThings = {band:"Rascal Flatts", food:"Pizza", person:"Bobby", book:"Great Expectations", movie:"Back to the Future", holiday:"Christmas"};
+
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+  favoriteThings.car = "Car";
+  favoriteThings.brand = "Prodigy";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
-
-
-
+  favoriteThings.food = "Lettuce";
+  favoriteThings.book = "50 Shades of Gray";
+  
 
 //NEXT PROBLEM
 
